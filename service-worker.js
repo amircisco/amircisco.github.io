72b54cc44ffa1f40af5816fedb249361e2e3ad7e
amir <amirjahani69@gmail.com>
2025-04-23 12:57:36 +0330

    Add files via upload
    append files

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'teb-cache-v1';
+const urlsToCache = [
+  '/teb/',
+  '/teb/index.html',
+  '/teb/manifest.json',
+  '/teb/static/js/main.chunk.js',
+  '/teb/static/js/0.chunk.js',
+  '/teb/static/js/bundle.js',
+  '/teb/static/css/main.chunk.css',
+  '/teb/icons/icon-192x192.png',
+  '/teb/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); 
